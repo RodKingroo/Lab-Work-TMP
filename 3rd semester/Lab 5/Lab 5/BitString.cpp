c50e523eb02e39068193389c684043fa0aea12c7
RodKingroo <49054722+RodKingroo@users.noreply.github.com>
2022-02-08 17:31:57 +0600

    Add files via upload

---
+++ 3rd semester/Lab 5/Lab 5/BitString.cpp
@@ -0,0 +1,21 @@
+#include "BitString.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	BitString a, b, c;
+	//cout << "Enter 1 BitString: " << endl;
+	//a.input();
+	a.FileInput("a.txt");
+	
+	cout << "Enter 2 BitString: " << endl;
+	b.input();
+
+	c = a.conjaction(b);
+	//c.output();
+	c.FileOutput("c.txt");
+	cout << "Result in c.txt" << endl;
+
+}

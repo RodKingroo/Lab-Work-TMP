149fd3525a770858487948d014b7545a23761547
RodKingroo <49054722+RodKingroo@users.noreply.github.com>
2022-02-08 20:22:40 +0600

    Add files via upload

---
+++ 4th semester/Lab 1/Lab 1/Buy.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Product.h"
+
+class Buy :public Product{
+private:
+	int sum;
+	double sum_price;
+	double sum_weight;
+
+public:
+	Buy();
+	void setSum(const int&);
+	int getSum()const;
+	double getSumPrice() const;
+	double getSumWeight() const;
+};
+

f1debda832a94dd320f79bc2c234d783d020da72
RodKingroo <49054722+RodKingroo@users.noreply.github.com>
2022-02-14 08:34:05 +0600

    Add files via upload

---
+++ 4th semester/Lab 3/Check.py
@@ -0,0 +1,13 @@
+from Buy import Buy
+
+class Check(Buy):
+    
+    def printProductInfo(self):
+        print("Товар: ", self.name)
+        print("Цена: ", self.price)
+        print("Вес: ", self.weight)
+    
+    def printBuyInfo(self):
+        print("Товара куплено: ", self.s, " шт.")
+        print("На сумму: ", self.sPrice, " руб.")
+        print("Общий вес: ", self.sWeight, " кг.")
---
+++ 4th semester/Lab 3/Source.py
@@ -0,0 +1,11 @@
+from Check import Check
+
+product=Check()
+product.name = '"Уральские" пельмени'
+product.price = 12
+product.weight = 1
+
+product.s = 30
+
+product.printProductInfo()
+product.printBuyInfo()
